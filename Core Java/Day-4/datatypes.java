9368f8d6329c0f4dc04163f7b596755d2649ac11
Bidyut <deepghosh369@gmail.com>
2024-08-24 11:33:59 +0530

    Learn About Data Types

---
+++ Core Java/Day-4/datatypes.java
@@ -0,0 +1,24 @@
+public class datatypes {
+    public static void main(String[] args) 
+    {
+        //Primitive Data type 
+
+        int myRollNum = 10; // int stores 4-bytes
+        float myFloatNum = 7.99f; // float stores 4-bytes
+        char myLetter ='D'; //char stores 2-bytes
+        boolean myBool = true; //boolean stores 1-bytes
+
+        //Non-Primitive Data Types
+
+        String myNameText = "Bidyut"; 
+
+
+        //Output
+        System.out.println(myRollNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myNameText);
+
+    }
+}
