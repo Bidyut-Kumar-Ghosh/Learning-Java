7f6a111935e0644140efc86541efdfd1c9d985dd
Bidyut <deepghosh369@gmail.com>
2024-08-27 11:50:53 +0530

    Try some operating in type casting

---
+++ Core Java/Day-5/example.java
@@ -0,0 +1,14 @@
+public class example 
+{
+    public static void main(String[] args) 
+    {
+        int maxScore = 500; //Maximum Possible Score 
+
+        int userScore = 412; // Score of User
+
+        float percentage = (float) userScore / maxScore * 100.0f; //Coverting score
+
+        System.out.println("User's Percentage is " + percentage);
+
+    }
+}
