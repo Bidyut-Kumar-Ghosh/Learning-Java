7bd87e2780b36879a63ea3a6cfe9cb6713dfd7dd
Bidyut <deepghosh369@gmail.com>
2024-08-27 11:46:15 +0530

    Learn about Type Casting and their types in java

---
+++ Core Java/Day-5/narrowcasting.java
@@ -0,0 +1,13 @@
+public class narrowcasting 
+{
+    public static void main(String[] args) 
+    {
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // Manual Casting : Double to Int
+
+        System.out.println(myDouble); //Output 9.78
+        System.out.println(myInt); //output 9
+
+    }
+    
+}
