15d4c52c7afe9a4e6266a3024b4a69659dee9ab6
Bidyut <deepghosh369@gmail.com>
2024-08-31 11:51:09 +0530

    trying sub,div,mul using operators

--- Core Java/Day-6/arithmetic.java
+++ Core Java/Day-6/arithmetic.java
@@ -4,7 +4,13 @@ public class arithmetic
     {
         int a = 566 , b = 566;
         int sum = a+b; //1132
+        int div = a/b; //1
+        int mul = a*b; //320356
+        int sub = a-b; //0
 
         System.out.println(sum);
+        System.out.println(div);
+        System.out.println(mul);
+        System.out.println(sub);
     }
 }
