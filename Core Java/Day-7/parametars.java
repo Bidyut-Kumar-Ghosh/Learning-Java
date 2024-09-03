b052e99a20e27ad3bce2b402aba2da6e3794f92a
Bidyut <deepghosh369@gmail.com>
2024-09-03 11:36:48 +0530

    Learning about parametars

---
+++ Core Java/Day-7/parametars.java
@@ -0,0 +1,13 @@
+public class parametars 
+{
+    static void printDetails(String name,int age)
+    {
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+    }
+    public static void main(String[] args) 
+    {
+        printDetails("Bidyut", 22);
+        printDetails("pankaj", 21);    
+    }    
+}
